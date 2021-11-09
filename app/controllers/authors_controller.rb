0b8b52a7e9f46954c3b9f3bf4d8ef1164e8fe37c
DevSchmidtchen <contact@schmidtchen.me>
2021-11-09 18:16:42 +0100

    show alan

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,11 +1,15 @@
 class AuthorsController < ApplicationController
+  def show
+    @author = Author.find(params[:id])
+  end
+
   def new
   end
 
   def create
     @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
- 
+
     @author.save
-    redirect_to root_path, notice: 'Success!'
+    redirect_to @author
   end
 end
