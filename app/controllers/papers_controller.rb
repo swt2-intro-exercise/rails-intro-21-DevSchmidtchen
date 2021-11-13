bf9d5128d270f9f4741afdc5f1d05711e9e6bf7b
DevSchmidtchen <contact@schmidtchen.me>
2021-11-13 11:51:21 +0100

    🤦‍♂️

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,7 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper..year(params[:year])
+    @papers = Paper.year(params[:year])
   end
 
   # GET /papers/1
